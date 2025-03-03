4e365c6a61a8cbe93383faf4b56a4917ef0213cc
Virginia Morera-Pujol <morera.virginia@gmail.com>
2025-03-03 09:21:22 +0000

    Update 6_populations_sdm.R
    Changes to run each model 10 times

--- 6_populations_sdm.R
+++ 6_populations_sdm.R
@@ -32,6 +32,7 @@ for(i in seq_along(pops_list)) {
   # i = 1
   temp <- pops_list[[i]]
   temp_sf <- temp %>% st_as_sf(coords = c("x", "y"))
+  
   pres.cov <- raster::extract(predictors_original, temp_sf)
   
   absence <- randomPoints(predictors_original, nrow(temp_sf))
@@ -56,35 +57,41 @@ names(pres_abs_list) <- pops_names
 model_list <- list()
 
 # for(i in seq_along(pres_abs_list)) {
-for(i in 14:17) {
+for(i in 1:17) {
   # i = 1
   all.cov <- pres_abs_list[[i]]
   
   xvars <-names(predictors_original)[-8]
   # remove lat lon for now and decide if we include it later
+  sdm_list <- list()
+  for (j in 1:10) {
+    print(paste("population", i, "out of 17 and iteration", j, "out of 10", sep = " "))
+    sdm <-bart.step(x.data = all.cov[,xvars],
+                    y.data = all.cov[,'pres'],
+                    full = TRUE,
+                    quiet = F, 
+                    iter.step = 100, 
+                    iter.plot = 100, 
+                    tree.step = 10)
+  sdm_list[j] <- list(sdm)}
   
-  sdm <-bart.step(x.data = all.cov[,xvars],
-                  y.data = all.cov[,'pres'],
-                  full = TRUE,
-                  quiet = F, 
-                  iter.step = 100, 
-                  iter.plot = 100, 
-                  tree.step = 10)
-  saveRDS(sdm, file = paste0("embarcadero_SDMs/outputs/", pops_names[i] ,"_model.RDS"))
-  
-  model_list[i] <- list(sdm)
-  
-  prediction <- predict(object = sdm,
-                        x.layers = predictors_original,
-                        quantiles =c(0.025, 0.975),
-                        splitby = 20,
-                        quiet = TRUE)
-  
-  names(prediction) <- c("Mean", "q2.5%", "q97.5%")
-  plot(prediction)
-  writeRaster(prediction, filename = paste0("embarcadero_SDMs/final_outputs/population_models/", pops_names[i] ,"_pred.grd"), 
-              overwrite = T)
+  saveRDS(sdm_list, file = paste0("embarcadero_SDMs/iter_outputs/", pops_names[i] ,"_model_list.RDS"))
 }
+          
+          
+#   model_list[i] <- list(sdm)
+#   
+#   prediction <- predict(object = sdm,
+#                         x.layers = predictors_original,
+#                         quantiles =c(0.025, 0.975),
+#                         splitby = 20,
+#                         quiet = TRUE)
+#   
+#   names(prediction) <- c("Mean", "q2.5%", "q97.5%")
+#   plot(prediction)
+#   writeRaster(prediction, filename = paste0("embarcadero_SDMs/final_outputs/population_models/", pops_names[i] ,"_pred.grd"), 
+#               overwrite = T)
+# }
 
 names(model_list) <- pops_names
 
@@ -95,6 +102,48 @@ model_list <- readRDS("embarcadero_SDMs/final_outputs/population_models/model_li
 # 3. Obtain outputs ####
 
 ### Variable importance ####
+
+pop_models <- list.files(path = "embarcadero_SDMs/iter_outputs/", pattern = "model_list.RDS", 
+                         full.names = T)
+pop_names <- str_sub(list.files(path = "embarcadero_SDMs/iter_outputs/", pattern = "model_list.RDS", 
+                         full.names = F), end = -16)
+
+
+pop_list <- list()
+for (i in seq_along(pop_models)){
+  # i = 1
+  
+  mod_list <- readRDS(pop_models[i]) 
+  iter_list <- list()
+  for (j in seq_along(mod_list))   {
+    varimp <- varimp(mod_list[[j]]) %>% 
+      arrange(-varimps) %>% 
+      mutate(iter = paste0("iter_", j), 
+             pop = pop_names[i])
+    
+    iter_list[j] <- list(varimp)
+  }
+  
+  pop_list[i] <- list(bind_rows(iter_list, id = NULL))
+
+}
+  
+
+all_varimps <- bind_rows(pop_list)
+
+varimps_sum <- all_varimps %>% 
+  separate(pop, into = c("Species", "Population"), sep = "_") %>% 
+  group_by(Species, Population, names) %>% 
+  summarise(mean = mean(varimps), 
+            sd = sd(varimps), 
+            iters = n())
+  
+
+ggplot(varimps_sum) + 
+  geom_point(aes(x = names, y = mean, alpha = iters)) + 
+  geom_errorbar(aes(x = names, ymin = mean-sd, ymax = mean+sd, alpha = iters)) +
+  facet_grid(Population~., scales = "free") + 
+  theme_bw()
 
 varimp_list <- list()
 for(i in seq_along(model_list)) {
